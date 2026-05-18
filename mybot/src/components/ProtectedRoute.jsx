5bb4c157d61ba69d6a75a886038f66692532120b
molomojc <molomojacob8@gmail.com>
2026-05-18 14:22:06 +0200

    restructure

--- mybot/src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export default function ProtectedRoute({ user }) {
-  if (!user) {
-    return <Navigate to="/login" replace />
-  }
-  return <Outlet />
-}
